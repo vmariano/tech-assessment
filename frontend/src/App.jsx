3f105edcd259d39cd87874daa491dda9a03560f4
Mariano <m.vicent@gmail.com>
2025-05-07 00:31:23 -0300

    can't fix console error 🤷

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+//TODO: this seems to throws a query client provider error, probably because is init with `undefined`
 import {QueryClient,  QueryClientProvider} from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import OrdersList from "./orders/Orders/OrdersList.jsx";
