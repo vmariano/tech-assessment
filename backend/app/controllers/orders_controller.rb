b14effe3128b457dd462179a1dbb195ee805044e
Mariano <m.vicent@gmail.com>
2025-05-13 01:44:09 -0300

    initial scaffolding

---
+++ backend/app/controllers/orders_controller.rb
@@ -0,0 +1,25 @@
+class OrdersController < ApplicationController
+
+  # @return all available orders
+  def index
+    @orders = Order.all
+    render json: @orders
+  end
+
+
+
+  def create
+    @order = Order.create(orders_params)
+    if @order.save
+      render json: @order
+    else
+      render json: { errors: @order.errors.full_messages }, status: 422
+    end
+  end
+
+  private
+
+  def orders_params
+    params.require(:order).permit(:name)
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  post "orders", to: "orders#create"
+  get "orders", to: "orders#index"
 end
