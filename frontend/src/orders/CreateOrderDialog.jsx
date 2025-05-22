26df090b90119039d43da1a858c703b89fd77b87
Mariano <m.vicent@gmail.com>
2025-05-22 04:07:50 -0300

    open dialog as modal

--- frontend/src/orders/CreateOrderDialog.jsx
+++ frontend/src/orders/CreateOrderDialog.jsx
@@ -1,9 +1,9 @@
 import {useCreateOrder} from "./api.js";
 import { ErrorBoundary } from "react-error-boundary";
+import {useRef} from "react";
 
 function CreateOrderDialog({dialogOpen, onClose}) {
     const createOrderMutation = useCreateOrder();
-
     const onSubmit = (form) => {
         const payload = {
             name: form.get("orderName"),
@@ -13,24 +13,32 @@ function CreateOrderDialog({dialogOpen, onClose}) {
         onClose();
     }
 
+    const openAsModal = (el) => {
+        if (dialogOpen) {
+            el?.showModal()
+        } else {
+            el?.close()
+        }
+    }
 
     return (
-        <dialog open={dialogOpen} >
-            <h3>Create a new order</h3>
+        <dialog  aria-modal="true" ref={openAsModal}>
             <ErrorBoundary fallback={
                 <p>
                     <p>There was an error while creating the order </p>
                     <button onClick={onClose}>Cancel</button>
                 </p>
             }>
-            <form method="dialog" action={onSubmit}>
-                <p><input type="text" placeholder="Order name" name="orderName" /></p>
-                <p><input type="text" placeholder="Description" name="description" /></p>
-                <button  type='submit' >Create</button>
-                <button onClick={onClose}>Cancel</button>
-            </form>
+                <form method="dialog" action={onSubmit}>
+                    <h3>Create a new order</h3>
+                    <p><input type="text" placeholder="Order name" name="orderName"/></p>
+                    <p><input type="text" placeholder="Description" name="description"/></p>
+                    <button type='submit'>Create</button>
+                    <button onClick={onClose}>Cancel</button>
+                </form>
             </ErrorBoundary>
         </dialog>
     )
 }
+
 export default CreateOrderDialog;
