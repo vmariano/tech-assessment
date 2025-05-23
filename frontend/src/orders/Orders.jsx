0640c73953aab9cfd324598a1e19f1ce82d67bca
Mariano <m.vicent@gmail.com>
2025-05-22 22:49:13 -0300

    add styles for add order.

--- frontend/src/orders/Orders.jsx
+++ frontend/src/orders/Orders.jsx
@@ -1,12 +1,15 @@
 import CreateOrderDialog from "./CreateOrderDialog.jsx";
 import OrdersList from "./OrdersList.jsx";
 import {useState} from "react";
+import './Orders.css';
 
 function Orders() {
     const [dialogOpen, setDialogOpen] = useState(false);
     return (
         <>
-            <button onClick={() => setDialogOpen(true)}>Add Order</button>
+            <div className="add-order-button">
+                <button onClick={() => setDialogOpen(true)}> ➕ Add Order</button>
+            </div>
             <CreateOrderDialog dialogOpen={dialogOpen} onClose={() => setDialogOpen(false)}/>
             <OrdersList />
         </>
